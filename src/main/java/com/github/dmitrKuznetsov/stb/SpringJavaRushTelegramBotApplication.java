22ad22be123611dc2d82a1ac0b42f6b3804367e5
dmitrKuznetsov <kuznetsov1995dima@mail.ru>
2022-05-17 11:07:01 +0300

    Spring Initializr

---
+++ src/main/java/com/github/dmitrKuznetsov/stb/SpringJavaRushTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitrKuznetsov.stb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaRushTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaRushTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dmitrKuznetsov/stb/SpringJavaRushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dmitrKuznetsov.stb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaRushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
